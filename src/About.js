1ef027740ae523510e8872cfeb79887c8a7586ff
nakhun9279 <fjfj9279@naver.com>
2020-08-31 08:14:39 +0900

    13.2.4 Route컴포넌트로 특정주소에 컴포넌트

--- src/About.js
+++ src/About.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
- 
+
+
 const App = () => {
   return (
     <div>
-      <Route path="/" component={Home} />
+      <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
     </div>
   );
 };
+
 
 
 export default App;
